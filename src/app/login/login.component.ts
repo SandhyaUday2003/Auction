618c1c3aaa6f033f644c20f6e547bea6c67f2116
Sandhyasaya2003 <sandhya.saya2003@gmail.com>
2024-12-12 16:42:43 +0530

    Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,20 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   user = {
-    username: '',
-    password: '',
+    email: '',
+    password: ''
   };
 
   onSubmit() {
-    if (this.user.username && this.user.password) {
-      console.log('Login successful:', this.user);
-      // Implement API call here
-    } else {
-      console.error('Form is invalid');
-    }
+    console.log('Login successful:', this.user);
   }
 }
